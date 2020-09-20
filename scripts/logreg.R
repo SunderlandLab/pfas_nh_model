44d7723a45cbdeec820093c216e08ec8614ea16f
Cindy Hu <CHu@mathematica-mpr.com>
2020-09-20 16:57:59 -0700

    feat: add an alternative logistic reg model, where impact of industry is characterized by counts

--- scripts/logreg.R
+++ scripts/logreg.R
@@ -7,7 +7,7 @@ library(MASS)
 
 # Load --------------------------------------------------------------------
 
-compounds_data <- readRDS('modeling_data/compounds_data.rds')
+compounds_data <- readRDS('../../modeling_data/compounds_data.rds')
 
 
 # Model -------------------------------------------------------------------
@@ -17,12 +17,36 @@ compounds_data <- readRDS('modeling_data/compounds_data.rds')
 # - Each list contains 1) model object; 2) training data; 3) test data; 4) predicted classes
 compounds_logreg <- map(compounds_data, function(data) {
   set.seed(123)
+  # drop station ID, reg, and impact3 (counts)
+  data<-data %>%
+    dplyr::select(-c(StationID, reg, matches("3$")))
   # Divide into training and test sets, 70-30%
   train_samples <- data$final %>% createDataPartition(p = 0.7, list = F)
   train_data <- data[train_samples, ]
   test_data <- data[-train_samples, ]
-  # Save model after stepwise regression, remove 'StationID' & 'reg' columns before fit
-  model <- glm(final ~ ., data = train_data[,-c(1:2)], family = binomial) %>% 
+  # Save model after stepwise regression
+  model <- glm(final ~ ., data = train_data, family = binomial) %>% 
+    stepAIC(trace = FALSE, direction = "both")
+  # Predict classes
+  probabilities <- model %>% predict(test_data, type = "response")
+  predicted_classes <- ifelse(probabilities > 0.5, 1, 0)
+  
+  return(list(model, train_data, test_data, predicted_classes) %>% 
+           set_names('model', 'train_data', 'test_data', 'predicted_classes'))
+})
+
+# test an alternative modeling where industry impact is characterized by counts, not sales volumes
+compounds_logreg_alt <- map(compounds_data, function(data) {
+  set.seed(123)
+  # drop station ID, reg, and impact2 (sales volume)
+  data<-data %>%
+    dplyr::select(-c(StationID, reg, matches("2$")))
+  # Divide into training and test sets, 70-30%
+  train_samples <- data$final %>% createDataPartition(p = 0.7, list = F)
+  train_data <- data[train_samples, ]
+  test_data <- data[-train_samples, ]
+  # Save model after stepwise regression
+  model <- glm(final ~ ., data = train_data, family = binomial) %>% 
     stepAIC(trace = FALSE, direction = "both")
   # Predict classes
   probabilities <- model %>% predict(test_data, type = "response")
@@ -35,7 +59,8 @@ compounds_logreg <- map(compounds_data, function(data) {
 
 # Save --------------------------------------------------------------------
 
-saveRDS(compounds_logreg, 'models/compounds_logreg.rds')
+saveRDS(compounds_logreg, '../../models/compounds_logreg.rds')
+saveRDS(compounds_logreg_alt, '../../models/compounds_logreg_alt.rds')
 
 
 
